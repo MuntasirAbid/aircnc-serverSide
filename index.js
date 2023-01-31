14cfe9ed33045825b690d0af89982bf3cbd109c9
MuntasirAbid <muntasira7@hotmail.com>
2023-01-31 17:50:20 +0100

    API generated to get all avtersiment by host email

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ const port = process.env.PORT || 8000
 app.use(cors())
 app.use(express.json())
 
+// Decode JWT
+// function verifyJWT(req, res, next) {
+//   const authHeader = req.headers.authorization
+
+//   if (!authHeader) {
+//     return res.status(401).send({ message: 'unauthorized access' })
+//   }
+//   const token = authHeader.split(' ')[1]
+
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+//     if (err) {
+//       return res.status(403).send({ message: 'Forbidden access' })
+//     }
+//     console.log(decoded)
+//     req.decoded = decoded
+//     next()
+//   })
+// }
+
 // Database Connection
 const uri = process.env.DB_URI
 const client = new MongoClient(uri, {
@@ -44,6 +63,15 @@ async function run() {
       res.send({ result, token })
     })
 
+    //Get all users
+
+    app.get('/users', async (req, res) => {
+
+      const users = await usersCollection.find().toArray()
+      console.log(users)
+      res.send(users)
+    })
+
     // Get a single user by Email
     app.get('/user/:email', async (req, res) => {
       const email = req.params.email
@@ -54,13 +82,43 @@ async function run() {
       res.send(user)
     })
 
-    //Get all users
+    // Get All Homes
+    app.get('/homes', async (req, res) => {
+      const query = {}
+      const cursor = homesCollection.find(query)
+      const homes = await cursor.toArray()
+      res.send(homes)
+    })
 
-    app.get('/users', async (req, res) => {
+    // Get All Homes for host
+    app.get('/homes/:email', async (req, res) => {
+      const email = req.params.email
+      // const decodedEmail = req.decoded.email
 
-      const users = await usersCollection.find().toArray()
-      console.log(users)
-      res.send(users)
+      // if (email !== decodedEmail) {
+      //   return res.status(403).send({ message: 'forbidden access' })
+      // }
+      const query = {
+        'host.email': email,
+      }
+      const cursor = homesCollection.find(query)
+      const homes = await cursor.toArray()
+      res.send(homes)
+    })
+
+    // Get Single Home
+    app.get('/home/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const home = await homesCollection.findOne(query)
+      res.send(home)
+    })
+
+    //Post a home
+    app.post('/homes', async (req, res) => {
+      const homes = req.body
+      const result = await homesCollection.insertOne(homes)
+      res.send(result)
     })
 
     //Save a booking
@@ -84,6 +142,8 @@ async function run() {
       console.log(bookings)
       res.send(bookings)
     })
+
+
 
     console.log('Database Connected...')
   } finally {
