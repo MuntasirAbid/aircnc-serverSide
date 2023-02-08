810128a4d5cfffa063c8cb6ce10b5212c420044a
MuntasirAbid <muntasira7@hotmail.com>
2023-02-08 23:36:49 +0100

    implemented API to delete booking

--- index.js
+++ index.js
@@ -179,6 +179,14 @@ async function run() {
       res.send(bookings)
     })
 
+    // Cancel booking
+    app.delete('/booking/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const result = bookingsCollection.deleteOne(query)
+      res.send(result)
+    })
+
 
 
     console.log('Database Connected...')
